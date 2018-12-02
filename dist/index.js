1504ec71be7fcc7790d756c7fa13014274c547bc
Stephan Thinagaran <stephan.thinagaran@live.com>
2018-12-02 12:23:41 +0530

    Bar chart working example added

--- dist/index.js
+++ dist/index.js
@@ -1,45 +1,172 @@
 "use strict";
-//Using Facade pattern
 var MyChart = /** @class */ (function () {
-    function MyChart() {
+    function MyChart(highChartOption) {
+        //this._container = container;
+        this._highChartOption = highChartOption;
     }
+    Object.defineProperty(MyChart.prototype, "myChart", {
+        get: function () {
+            return this._myChart;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    MyChart.prototype.BuildChart = function () {
+        this._myChart = Highcharts.chart(this._highChartOption);
+    };
     return MyChart;
 }());
-var ChartOption = /** @class */ (function () {
-    function ChartOption(renderTo, type) {
+var ChartConfigOption = /** @class */ (function () {
+    function ChartConfigOption(chart, title, xAxis, yAxis, series) {
+        this._chart = chart;
+        this._title = title;
+        this._xAxis = xAxis;
+        this._yAxis = yAxis;
+        this._series = series;
+    }
+    ChartConfigOption.prototype.GetChartOptions = function () {
+        var options = {
+            chart: {
+                renderTo: this._chart.renderTo,
+                type: this._chart.type
+            },
+            title: {
+                text: this._title.text
+            },
+            xAxis: {
+                categories: this._xAxis.categories
+            },
+            yAxis: {
+                title: {
+                    text: this._yAxis._title.text
+                }
+            },
+            series: this._series.data
+        };
+        return options;
+    };
+    return ChartConfigOption;
+}());
+var ChartOptions = /** @class */ (function () {
+    function ChartOptions(renderTo, type) {
         this._renderTo = renderTo;
         this._type = type;
     }
-    return ChartOption;
+    Object.defineProperty(ChartOptions.prototype, "renderTo", {
+        get: function () {
+            return this._renderTo;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ChartOptions.prototype, "type", {
+        get: function () {
+            return this._type;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return ChartOptions;
 }());
-var TitleOption = /** @class */ (function () {
-    function TitleOption(text) {
+var TitleOptions = /** @class */ (function () {
+    function TitleOptions(text) {
         this._text = text;
     }
-    return TitleOption;
+    Object.defineProperty(TitleOptions.prototype, "text", {
+        get: function () {
+            return this._text;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return TitleOptions;
 }());
 var XAxisOptions = /** @class */ (function () {
     function XAxisOptions(categories) {
         this._categories = categories;
     }
+    Object.defineProperty(XAxisOptions.prototype, "categories", {
+        get: function () {
+            return this._categories;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return XAxisOptions;
 }());
 var YAXisOptions = /** @class */ (function () {
     function YAXisOptions(YAXisTitleOptions) {
         this._title = YAXisTitleOptions;
     }
+    Object.defineProperty(YAXisOptions.prototype, "title", {
+        get: function () {
+            return this._title;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return YAXisOptions;
 }());
 var YAXisTitleOptions = /** @class */ (function () {
     function YAXisTitleOptions(text) {
         this._text = text;
     }
+    Object.defineProperty(YAXisTitleOptions.prototype, "text", {
+        get: function () {
+            return this._text;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return YAXisTitleOptions;
 }());
 var SeriesOptions = /** @class */ (function () {
     function SeriesOptions(data) {
         this._data = data;
     }
+    Object.defineProperty(SeriesOptions.prototype, "data", {
+        get: function () {
+            return this._data;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return SeriesOptions;
 }());
+var BuildSampleBarChart = /** @class */ (function () {
+    function BuildSampleBarChart() {
+    }
+    BuildSampleBarChart.prototype.Build = function () {
+        var renderTo = "container";
+        var chartType = "bar";
+        var chartOptions = new ChartOptions(renderTo, chartType);
+        var chartTitleText = "Fruit Consumption";
+        var titleOptions = new TitleOptions(chartTitleText);
+        var xAxisCategories = ["Apples", "Bananas", "Oranges"];
+        var xAxisOptions = new XAxisOptions(xAxisCategories);
+        var yaxisTitleText = "Fruit eaten";
+        var yAxisTitleOptions = new YAXisTitleOptions(yaxisTitleText);
+        var yAxisOptions = new YAXisOptions(yAxisTitleOptions);
+        var seriesData = [
+            {
+                name: "Jane",
+                data: [1, 0, 4]
+            },
+            {
+                name: "John",
+                data: [5, 7, 3]
+            }
+        ];
+        var seriesOptions = new SeriesOptions(seriesData);
+        var chartConfigOptions = new ChartConfigOption(chartOptions, titleOptions, xAxisOptions, yAxisOptions, seriesOptions);
+        var highchartOption = chartConfigOptions.GetChartOptions();
+        var myBarChart = new MyChart(highchartOption);
+        myBarChart.BuildChart();
+    };
+    return BuildSampleBarChart;
+}());
+$(document).ready(function () {
+    var buildSampleChart = new BuildSampleBarChart();
+    buildSampleChart.Build();
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,82 @@
 //Using Facade pattern
+declare var Highcharts: any;
 
-class MyChart {}
+class MyChart {
+  private _myChart: any;
+  //private _container: string;
+  private _highChartOption: object;
 
-class ChartOption {
+  public get myChart(): string {
+    return this._myChart;
+  }
+
+  constructor(highChartOption: object) {
+    //this._container = container;
+    this._highChartOption = highChartOption;
+  }
+
+  public BuildChart(): void {
+    this._myChart = Highcharts.chart(this._highChartOption);
+  }
+}
+
+class ChartConfigOption {
+  private _chart: ChartOptions;
+  private _title: TitleOptions;
+  private _xAxis: XAxisOptions;
+  private _yAxis: YAXisOptions;
+  private _series: SeriesOptions;
+
+  constructor(
+    chart: ChartOptions,
+    title: TitleOptions,
+    xAxis: XAxisOptions,
+    yAxis: YAXisOptions,
+    series: SeriesOptions
+  ) {
+    this._chart = chart;
+    this._title = title;
+    this._xAxis = xAxis;
+    this._yAxis = yAxis;
+    this._series = series;
+  }
+
+  public GetChartOptions(): object {
+
+    let options = {
+      chart: {
+        renderTo: this._chart.renderTo,
+        type: this._chart.type
+      },
+      title: {
+        text: this._title.text
+      },
+      xAxis: {
+        categories: this._xAxis.categories
+      },
+      yAxis: {
+        title: {
+          text: this._yAxis._title.text
+        }
+      },
+      series: this._series.data
+    };
+
+    return options;
+  }
+}
+
+class ChartOptions {
   private _renderTo: string;
   private _type: string;
+
+  public get renderTo(): string {
+    return this._renderTo;
+  }
+
+  public get type(): string {
+    return this._type;
+  }
 
   constructor(renderTo: string, type: string) {
     this._renderTo = renderTo;
@@ -12,8 +84,12 @@ class ChartOption {
   }
 }
 
-class TitleOption {
+class TitleOptions {
   private _text: string;
+
+  public get text(): string {
+    return this._text;
+  }
 
   constructor(text: string) {
     this._text = text;
@@ -23,6 +99,10 @@ class TitleOption {
 class XAxisOptions {
   private _categories: Array<string>;
 
+  public get categories(): Array<string> {
+    return this._categories;
+  }
+
   constructor(categories: Array<string>) {
     this._categories = categories;
   }
@@ -31,6 +111,10 @@ class XAxisOptions {
 class YAXisOptions {
   public _title: YAXisTitleOptions;
 
+  public get title(): YAXisTitleOptions {
+    return this._title;
+  }
+
   constructor(YAXisTitleOptions: YAXisTitleOptions) {
     this._title = YAXisTitleOptions;
   }
@@ -38,6 +122,11 @@ class YAXisOptions {
 
 class YAXisTitleOptions {
   private _text: string;
+
+  public get text(): string {
+    return this._text;
+  }
+
   constructor(text: string) {
     this._text = text;
   }
@@ -49,5 +138,57 @@ class SeriesOptions {
   constructor(data: Array<object>) {
     this._data = data;
   }
+
+  public get data(): Array<object> {
+    return this._data;
+  }
 }
 
+class BuildSampleBarChart {
+  constructor() {}
+
+  public Build(): void {
+    let renderTo: string = "container";
+    let chartType: string = "bar";
+    let chartOptions = new ChartOptions(renderTo, chartType);
+
+    let chartTitleText: string = "Fruit Consumption";
+    let titleOptions = new TitleOptions(chartTitleText);
+
+    let xAxisCategories: Array<string> = ["Apples", "Bananas", "Oranges"];
+    let xAxisOptions = new XAxisOptions(xAxisCategories);
+
+    let yaxisTitleText: string = "Fruit eaten";
+    let yAxisTitleOptions = new YAXisTitleOptions(yaxisTitleText);
+    let yAxisOptions = new YAXisOptions(yAxisTitleOptions);
+
+    let seriesData: Array<object> = [
+      {
+        name: "Jane",
+        data: [1, 0, 4]
+      },
+      {
+        name: "John",
+        data: [5, 7, 3]
+      }
+    ];
+    let seriesOptions = new SeriesOptions(seriesData);
+
+    let chartConfigOptions = new ChartConfigOption(
+      chartOptions,
+      titleOptions,
+      xAxisOptions,
+      yAxisOptions,
+      seriesOptions
+    );
+    let highchartOption = chartConfigOptions.GetChartOptions();
+
+    let myBarChart = new MyChart(highchartOption);
+    myBarChart.BuildChart();
+  }
+}
+
+$(document).ready(function() {
+  let buildSampleChart = new BuildSampleBarChart();
+  buildSampleChart.Build();
+});
